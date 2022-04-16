a9b154138fffdc722f9e068e848aa5e43340862c
ykx <fadwad@foxmail.com>
2022-04-16 17:45:16 +0800

    add zsh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ ln -sf dotfiles/gitconfig $HOME/.gitconfig
 ln -sf dotfiles/bashrc $HOME/.bashrc
 ln -sf dotfiles/vimrc $HOME/.vimrc
 ln -sf dotfiles/zshrc $HOME/.zshrc
+if [ -d $HOME/dotfiles/oh-my-zsh ];then
+	echo 'oh-my-zsh exists'
+else 
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/dotfiles/oh-my-zsh
+fi
