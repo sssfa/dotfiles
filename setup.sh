b39c421b4794d1a67a441d0b8d2f96ba13578cee
ykx <fadwad@foxmail.com>
2022-04-16 19:10:58 +0800

    modifed

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #! /usr/bin/bash
 #if [-f ]
+ln -sf dotfiles/tmux.conf $HOME/.tmux.conf
 ln -sf dotfiles/gitconfig $HOME/.gitconfig
 ln -sf dotfiles/bashrc $HOME/.bashrc
 ln -sf dotfiles/vimrc $HOME/.vimrc
 ln -sf dotfiles/zshrc $HOME/.zshrc
-if [ -d $HOME/dotfiles/oh-my-zsh ];then
+if [ -d $HOME/.oh-my-zsh ];then
 	echo 'oh-my-zsh exists'
 else 
-git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/dotfiles/oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 fi
+
+if [ -d $HOME/.oh-my-zsh/plugins/zsh-autosuggestions ];then
+	echo 'already have'
+else
+	mkdir $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+fi
+	
